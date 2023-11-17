0fed70fec79b282bf4ec6b023d10905c940ca330
JotaJota96 <jjap96@gmail.com>
2023-11-16 21:57:39 -0300

    hecho responder mensajes de texto
    Al recibir un mensaje de texto responde citando el mensaje recibido
    Si el mensaje no es de texto responde otra cosa

--- README.md
+++ README.md
@@ -8,5 +8,5 @@ Este proyecto es una prueba para aprender cómo crear un chatbot para WhatsApp u
 - [X] **Enviar Mensajes de Texto:** Enviar un mensaje personalizado a un usuario que haya iniciado la conversación previamente.
 - [X] **Configurar el Webhook:** Establecer una conexión para recibir las notificaciones de eventos y mensajes.
 - [X] **Recibir Mensajes:** Identificar el tipo de mensaje recibido y acceder a su contenido si es de texto.
-- [ ] **Responder Mensajes de Texto:** Responder a un mensaje de texto incluyendo el contenido del mensaje recibido.
+- [X] **Responder Mensajes de Texto:** Responder a un mensaje de texto incluyendo el contenido del mensaje recibido.
 - [ ] **Solicitar datos uno a uno:** Solicitar una serie de datos a un usuario y mostrarlos en un mensaje al finalizar.
--- src/main/java/io/JotaJota96/WhatsAppBotPrototype/services/ReceptionService.java
+++ src/main/java/io/JotaJota96/WhatsAppBotPrototype/services/ReceptionService.java
@@ -36,7 +36,7 @@ public class ReceptionService {
             if ("text".equalsIgnoreCase(message.type())) {
                 this.handleTextMessage(message);
             } else {
-                System.out.println("Mensajes de tipo '" + message.type() + "' no soportados");
+                this.handleUnsupportedMessage(message);
             }
         });
     }
@@ -48,6 +48,39 @@ public class ReceptionService {
      */
     private void handleTextMessage(MessageDTO message) {
         System.out.println("Mensaje recibido: " + message.text().body());
+        // Inicio de mensaje aleatorio (ej: "Eco del mensaje: ", "Tu mensaje fue: ", etc)
+        String response = switch ((int) (Math.random() * 5)) {
+            case 0 -> "Eco del mensaje: ";
+            case 1 -> "Tu mensaje fue: ";
+            case 2 -> "Mensaje recibido: ";
+            case 3 -> "Has enviado: ";
+            case 4 -> "Mensaje: ";
+            default -> "";
+        };
+
+        response += message.text().body();
+
+        // Envío la respuesta
+        sendingService.sendTextMessage(message.from(), response);
+    }
+
+    /**
+     * Este método se encarga de manejar los tipos de mensajes no soportados
+     *
+     * @param message Mensaje de cualquier tipo
+     */
+    private void handleUnsupportedMessage(MessageDTO message) {
+        System.out.println("Mensajes de tipo '" + message.type() + "' no soportados");
+        // mensaje aleatorio de respuesta
+        String response = switch ((int) (Math.random() * 3)) {
+            case 0 -> "Por el momento no soporto mensajes de tipo '" + message.type() + "'...";
+            case 1 -> "No sé que hacer con mensajes que no sean de texto...";
+            case 2 -> "Lo siento, no te entiendo, prueba con un mensaje de texto.";
+            default -> "";
+        };
+
+        // Envío la respuesta
+        sendingService.sendTextMessage(message.from(), response);
     }
 
 }
